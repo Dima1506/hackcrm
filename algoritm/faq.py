7ee804f4794f4debd29b2b1b47ec2848b79cfd73
Dima1506 <ichetkin.d@haclever.org>
2021-08-22 12:45:07 +0300

    ff2

--- algoritm/faq.py
+++ algoritm/faq.py
@@ -5,7 +5,7 @@ import re
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 from navec import Navec
-import re
+import random
 
 path = 'navec_hudlit_v1_12B_500K_300d_100q.tar'
 model = Navec.load(path)
@@ -37,6 +37,10 @@ def sentence_middle_avg(words_mas, vector, num_features = 300):
     return featureVec
 
 def make_all_vectorwords(words, types):
+    '''for s in words:
+        t = sentence_avg_vector(s.lower())
+        if 0 in t:
+            print(s, t)'''
     vector_arr = np.array([sentence_avg_vector(s.lower()) for s in words])
     print('sd')
     np.save(types, vector_arr)
@@ -61,15 +65,16 @@ def sentence_faq(sentence):
 
 
 def parse_xlsx(sentence_json):
-    wb = load_workbook('text.xlsx')
+    wb = load_workbook('text3.xlsx')
     ws = wb.active
-    print(ws.max_column)
+    #print(ws.max_column)
     column = ws.max_column
     mass = []
     types=''
     print(get_column_letter(column))
     column = ws[get_column_letter(column)]
     id_sentence = -1
+    print(len(column))
     for i in range(len(column)):
         if i == 0:
             types=column[i].value
@@ -91,14 +96,17 @@ def parse_xlsx(sentence_json):
                 else:
                     strp+=char
                 pred_element = char
-            print(id_sentence)
+            #print(id_sentence, column[i].value)
             sentence_json['end_id_sentence'].append(id_sentence)
             #print(mass)
             #mass = mass + re.split("r'[ .]+", column[i].value)
             #mass.append(.split())
         sentence_json['text']=mass
-    print(mass)
-    print(types)
+    for i in range(len(mass)):
+        if mass[i][0:1]=='':
+            print(mass[i],mass[i+1])
+    #print(mass)
+    #print(types)
     make_all_vectorwords(mass,types)
     avg_class(sentence_json)
     return sentence_json
@@ -139,6 +147,7 @@ def avg_class(sentence_json):
                                 sentence_json['sentence_avg'][sentence_json['class_avg'][str(i)]] = mas_vremen
                                 sentence_json['class_avg'][str(j)]=str(sentence_json['class_avg'][str(i)])
                                 print('1:'+sentence_json['text'][i]+'\n'+'2:'+sentence_json['text'][j])
+                                print('1bug:'+sentence_json['text'][i+1]+'\n'+'2bug:'+sentence_json['text'][j+1])
                     elif str(j) in sentence_json['class_avg']:
                         massiv_class = sentence_json['sentence_avg'][sentence_json['class_avg'][str(j)]]
                         #print('sss2 '+str(massiv_class))
@@ -151,9 +160,11 @@ def avg_class(sentence_json):
                                 #print(mas_vremen)
                                 mas_vremen.append(i)
                                 #print(i)
+
                                 sentence_json['sentence_avg'][sentence_json['class_avg'][str(j)]] = mas_vremen
                                 sentence_json['class_avg'][str(i)]=str(sentence_json['class_avg'][str(j)])
                                 print('1:'+sentence_json['text'][i]+'\n'+'2:'+sentence_json['text'][j])
+                                print('1bug:'+sentence_json['text'][i+1]+'\n'+'2bug:'+sentence_json['text'][j+1])
                     else:
                         ka = 1
                         sentence_json['class_avg'][str(i)]=str(class_number)
@@ -162,6 +173,7 @@ def avg_class(sentence_json):
                         class_number+=1
                         #exclude_mas.append(j)
                         print('1:'+sentence_json['text'][i]+'\n'+'2:'+sentence_json['text'][j])
+                        print('1bug:'+sentence_json['text'][i+1]+'\n'+'2bug:'+sentence_json['text'][j+1])
                         #print(sen1_sen2_similarity)
                         #print(i,j)
         if ka == 0:
@@ -171,32 +183,67 @@ def avg_class(sentence_json):
     return sentence_json
 
         
-def markov_cep(sentence_json):
-    sentence_json['sentence_avg']['inter']= sentence_json['zero_id_sentence']
-    sentence_json['sentence_avg']['end']= sentence_json['zero_id_sentence']
-    sentence_json['sentence_markov']['0'] = {}
-    for i in sentence_json['zero_id_sentence']:
-        class_i = sentence_json['class_avg'][str(i)]
-        if str(class_i) in sentence_json['sentence_markov']['0']:
-            strp = sentence_json['sentence_markov']['0'].at(str(class_i))+1
-            sentence_json['sentence_markov']['0'][str(class_i)]=strp
+def markov_form_service(sentence_json):
+    #sentence_json['sentence_avg']['inter']= sentence_json['zero_id_sentence']
+    #sentence_json['sentence_avg']['end']= sentence_json['zero_id_sentence']
+    sentence_json['sentence_markov']['start'] = {'next_sent':[]}
+    for i in range(len(sentence_json['text'])):
+        if i in sentence_json['zero_id_sentence']:
+            sentence_json['sentence_markov'][sentence_json['class_avg'][str(i)]] = {
+            'sent':sentence_json['sentence_avg'][sentence_json['class_avg'][str(i)]],
+                'next_sent':[]
+            }
+            for j in sentence_json['sentence_markov'][sentence_json['class_avg'][str(i)]]['sent']:
+                sentence_json['sentence_markov'][sentence_json['class_avg'][str(i)]]['next_sent'].append(sentence_json['class_avg'][str(j+1)])
+            sentence_json['sentence_markov']['start']['next_sent'].append(sentence_json['class_avg'][str(i)])
+        elif i in sentence_json['end_id_sentence']:
+            if sentence_json['class_avg'][str(i)] not in sentence_json['sentence_markov']:
+                sentence_json['sentence_markov'][sentence_json['class_avg'][str(i)]] = {
+                'sent':sentence_json['sentence_avg'][sentence_json['class_avg'][str(i)]],
+                    'next_sent':['end']
+                }
+            else:
+                #sentence_json['sentence_markov'][sentence_json['class_avg'][str(i)]]['sent'].append(str(i))
+                sentence_json['sentence_markov'][sentence_json['class_avg'][str(i)]]['next_sent'].append('end')
         else:
-            sentence_json['sentence_markov']['0'][str(class_i)] = 1
+            sentence_json['sentence_markov'][sentence_json['class_avg'][str(i)]] = {
+            'sent':sentence_json['sentence_avg'][sentence_json['class_avg'][str(i)]],
+                'next_sent':[]
+            }
+            for j in sentence_json['sentence_markov'][sentence_json['class_avg'][str(i)]]['sent']:
+                sentence_json['sentence_markov'][sentence_json['class_avg'][str(i)]]['next_sent'].append(sentence_json['class_avg'][str(j+1)])
+            #sentence_json['sentence_markov']['start']['next_sent'].append(sentence_json['class_avg'][str(i)])
     print(sentence_json['sentence_markov']['0'])
     return sentence_json
+
+def markov_cep(sentence_json):
+    class_s = random.choice(sentence_json['sentence_markov']['start']['next_sent'])
+    text = ''
+    while True:
+        num_sentences = random.choice(sentence_json['sentence_markov'][class_s]['sent'])
+        class_new = random.choice(sentence_json['sentence_markov'][class_s]['next_sent'])
+        text += sentence_json['text'][num_sentences]
+        class_s = class_new
+        if class_s == 'end':
+            break
+    return text
 
 
 
 #make_all_vectorwords()
 #print(sentence_faq('хочу отдохнуть'))
-'''sentence_json={'sentence_markov':{},'sentences':[], 'sentence_avg':{}, 'zero_id_sentence':[],'class_avg':{},'end_id_sentence':[]}
+sentence_json={'sentence_markov':{},'sentences':[], 'sentence_avg':{}, 'zero_id_sentence':[],'class_avg':{},'end_id_sentence':[]}
 sentence_json = parse_xlsx(sentence_json)
 f = open("data.txt", "w")
 f.write(str(sentence_json))
-f.close()'''
+f.close()
 f = open("data.txt", "r")
 sentence_json = eval(f.read())
 f.close()
-sentence_json = markov_cep(sentence_json)
-print(sentence_json)
+sentence_json = markov_form_service(sentence_json)
+f = open("data.txt", "w")
+f.write(str(sentence_json))
+f.close()
+print(markov_cep(sentence_json))
+#print(sentence_json)
 
